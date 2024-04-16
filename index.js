d4ca49368c8ef77aa63a0d1b9b4f50e59d008fc1
magnus5552 <isackov.stepa@yandex.ru>
2024-04-16 17:29:19 +0500

    Add single drink form

---
+++ index.js
@@ -0,0 +1,159 @@
+function drinkForm({ drinks }) {}
+
+function singleDrinkForm({ id, types, milk, options }) {
+  let fieldSet = create(
+    "fieldset",
+    { className: "beverage" },
+    create("h4", (props = { className: "beverage-count" }), `Напиток №${id}`),
+    create(
+      "label",
+      { className: "field" },
+      create("span", { className: "label-text" }, "Я буду"),
+      create(
+        "select",
+        {},
+        ...types.values.map((o) =>
+          option(
+            { value: o.value, selected: o.value === types.selected },
+            (v) => (types.selected = v),
+            create("span", {}, o.text)
+          )
+        )
+      )
+    ),
+    checkboxList("radio", milk, (props, v) => (props.selected = v), (props, v) => props.selected === v),
+    checkboxList("checkbox", options, (props, v) =>
+      props.selected.includes(v)
+        ? props.selected.remove(v)
+        : props.selected.push(v),
+        (props, v) => props.selected.includes(v)
+    )
+  );
+  return fieldSet;
+}
+
+function checkboxList(inputType, props, changeSelected, checkSelected) {
+  return create(
+    "div",
+    { className: "field" },
+    checkboxLabel(props.label),
+    ...props.values.map((o) =>
+      create(
+        "label",
+        { className: "checkbox-field" },
+        input(
+          {
+            type: inputType,
+            name: props.name,
+            value: o.value,
+            checked: checkSelected(props, o.value),
+          },
+          (v) => changeSelected(props, v)
+        ),
+        create("span", {}, o.text)
+      )
+    )
+  );
+}
+
+function checkboxLabel(text) {
+  return create("span", { className: "checkbox-label" }, text);
+}
+
+/**
+ * @param {HTMLElementTagNameMap['input']} props
+ * @param {Function} onCheck
+ */
+function input(props, onCheck, ...children) {
+  let elem = create("input", props, ...children);
+  elem.addEventListener("changed", (e) => onCheck(e.target.value));
+  return elem;
+}
+
+/**
+ * @param {HTMLElementTagNameMap['option']} props
+ * @param {Function} onCheck
+ */
+function option(props, onCheck, ...children) {
+  let elem = create("option", props, ...children);
+  elem.addEventListener("changed", (e) => onCheck(e.target.value));
+  return elem;
+}
+
+/**'
+ * @param {keyof HTMLElementTagNameMap} tagName
+ * @param {HTMLElementTagNameMap[tagName]} props
+ * @param {...HTMLElement[]} children
+ * @returns {HTMLElementTagNameMap[tagName]}
+ */
+function create(tagName, props, ...children) {
+  let elem = document.createElement(tagName);
+  Object.keys(props).forEach(x => (elem[x] = props[x]));
+  elem.append(...children);
+  return elem;
+}
+
+const defaultDrink = {
+    types: {
+      values: [
+        {
+          value: "espresso",
+          text: "Эспрессо",
+        },
+        {
+          value: "capuccino",
+          text: "Капучино",
+        },
+        {
+          value: "cacao",
+          text: "Какао",
+        },
+      ],
+      selected: "capuccino",
+    },
+    milk: {
+      label: 'Сделайте напиток на',
+      name: 'milk',
+      values: [
+        {
+          value: "usual",
+          text: "обычном молоке",
+        },
+        {
+          value: "no-fat",
+          text: "обезжиренном молоке",
+        },
+        {
+          value: "soy",
+          text: "соевом молоке",
+        },
+        {
+          value: "coconut",
+          text: "кокосовом молоке",
+        }
+      ],
+      selected: "usual",
+    },
+    options: {
+      label: 'Добавьте к напитку:',
+      name: 'options',
+      values: [
+        {
+          value: "espresso",
+          text: "Эспрессо",
+        },
+        {
+          value: "capuccino",
+          text: "Капучино",
+        },
+        {
+          value: "cacao",
+          text: "Какао",
+        },
+      ],
+      selected: ["capuccino"],
+    },
+  };
+drinks = [defaultDrink];
+
+document.querySelector('#test').appendChild(singleDrinkForm({id: 1, ...defaultDrink}));
