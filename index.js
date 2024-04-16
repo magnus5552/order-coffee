5d2942a7bc62952a256ff6677a4238898e196559
antoshkaxxr <63500718+antoshkaxxr@users.noreply.github.com>
2024-04-16 19:10:45 +0500

    task 9

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
   let modal = document.querySelector(".modal");
   let modalContent = document.querySelector(".modal-content");
   let addButton = document.querySelector(".add-button");
-  let modalClose = document.querySelector(".close");
   let beverageCount = 1;
 
   addButton.addEventListener("click", function () {
@@ -20,8 +19,35 @@ document.addEventListener("DOMContentLoaded", function () {
     let orderText = `Заказ принят! Вы заказали ${totalBeverageCount} ${getCorrectWordForm(totalBeverageCount, 'напиток')}`;
     let tableContent = generateOrderTableContent(beverages);
 
-    modalContent.innerHTML = `<p>${orderText}</p>${tableContent}`;
+    modalContent.innerHTML += `<p>${orderText}</p>${tableContent}`;
+    modalContent.innerHTML += `
+            <br>
+            <label for="order-time">Выберите время заказа:</label>
+            <input type="time" id="order-time">
+            <br>
+            <br>
+            <button class="submit-order-button">Оформить</button>
+        `;
     modal.style.display = 'block';
+
+    document.querySelector('.submit-order-button').addEventListener('click', function() {
+      let orderTime = document.getElementById('order-time').value;
+      let currentTime = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+
+      if (orderTime < currentTime) {
+        alert("Мы не умеем перемещаться во времени. Выберите время позже, чем текущее.");
+        document.getElementById('order-time').style.border = "2px solid red";
+      } else {
+        document.getElementById('order-time').style.border = "2px solid green"
+        modal.style.display = 'none';
+        location.reload();
+      }
+    });
+
+    document.querySelector(".close-modal").addEventListener("click", function () {
+      modal.style.display = 'none';
+      location.reload();
+    });
   });
 
   function createBeverageFieldset(beverageCount) {
@@ -118,16 +144,16 @@ document.addEventListener("DOMContentLoaded", function () {
     beverages.forEach((beverage) => {
       let selectedMilk = beverage.querySelector(`input[type="radio"]:checked`).nextElementSibling.textContent;
       if (selectedMilk === 'обычном молоке') {
-        selectedMilk = 'обычное молоко';
+        selectedMilk = 'обычное';
       }
       if (selectedMilk === 'обезжиренном молоке') {
-        selectedMilk = 'обезжиренное молоко';
+        selectedMilk = 'обезжиренное';
       }
       if (selectedMilk === 'соевом молоке') {
-        selectedMilk = 'соевое молоко';
+        selectedMilk = 'соевое';
       }
       if (selectedMilk === 'кокосовом молоке') {
-        selectedMilk = 'кокосовое молоко';
+        selectedMilk = 'кокосовое';
       }
 
       let selectedOptions = [];
