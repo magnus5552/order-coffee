24d237950c6bd6cf98827b96b4360d5a9b949846
magnus5552 <isackov.stepa@yandex.ru>
2024-04-16 18:22:08 +0500

    not working :(

--- index.js
+++ index.js
@@ -1,126 +1,3 @@
-function drinkForm({ drinks }) {
-  return create(
-    "form",
-    {},
-    ...drinks.map((o, i) => singleDrinkForm({ id: i + 1, ...o })),
-    create(
-      "div",
-      {},
-      create(
-        "button",
-        { type: "button", className: "add-button" },
-        "+ Добавить напиток"
-      )
-    ),
-    create(
-      "div",
-      { style: "margin-top: 30px" },
-      create("button", { type: "submit", className: "submit-button" }, "Готово")
-    )
-  );
-}
-
-function singleDrinkForm({ id, types, milk, options }) {
-  let fieldSet = create(
-    "fieldset",
-    { className: "beverage" },
-    create("h4", (props = { className: "beverage-count" }), `Напиток №${id}`),
-    create(
-      "label",
-      { className: "field" },
-      create("span", { className: "label-text" }, "Я буду"),
-      create(
-        "select",
-        {},
-        ...types.values.map((o) =>
-          option(
-            { value: o.value, selected: o.value === types.selected },
-            (v) => (types.selected = v),
-            create("span", {}, o.text)
-          )
-        )
-      )
-    ),
-    checkboxList(
-      "radio",
-      milk,
-      (props, v) => (props.selected = v),
-      (props, v) => props.selected === v
-    ),
-    checkboxList(
-      "checkbox",
-      options,
-      (props, v) =>
-        props.selected.includes(v)
-          ? props.selected.remove(v)
-          : props.selected.push(v),
-      (props, v) => props.selected.includes(v)
-    )
-  );
-  return fieldSet;
-}
-
-function checkboxList(inputType, props, changeSelected, checkSelected) {
-  return create(
-    "div",
-    { className: "field" },
-    checkboxLabel(props.label),
-    ...props.values.map((o) =>
-      create(
-        "label",
-        { className: "checkbox-field" },
-        input(
-          {
-            type: inputType,
-            name: props.name,
-            value: o.value,
-            checked: checkSelected(props, o.value),
-          },
-          (v) => changeSelected(props, v)
-        ),
-        create("span", {}, o.text)
-      )
-    )
-  );
-}
-
-function checkboxLabel(text) {
-  return create("span", { className: "checkbox-label" }, text);
-}
-
-/**
- * @param {HTMLElementTagNameMap['input']} props
- * @param {Function} onCheck
- */
-function input(props, onCheck, ...children) {
-  let elem = create("input", props, ...children);
-  elem.addEventListener("changed", (e) => onCheck(e.target.value));
-  return elem;
-}
-
-/**
- * @param {HTMLElementTagNameMap['option']} props
- * @param {Function} onCheck
- */
-function option(props, onCheck, ...children) {
-  let elem = create("option", props, ...children);
-  elem.addEventListener("changed", (e) => onCheck(e.target.value));
-  return elem;
-}
-
-/**'
- * @param {keyof HTMLElementTagNameMap} tagName
- * @param {HTMLElementTagNameMap[tagName]} props
- * @param {...HTMLElement[]} children
- * @returns {HTMLElementTagNameMap[tagName]}
- */
-function create(tagName, props, ...children) {
-  let elem = document.createElement(tagName);
-  Object.keys(props).forEach((x) => (elem[x] = props[x]));
-  elem.append(...children);
-  return elem;
-}
-
 const defaultDrink = {
   types: {
     values: [
@@ -182,7 +59,140 @@ const defaultDrink = {
     selected: ["capuccino"],
   },
 };
-drinks = [defaultDrink];
 
-document.querySelector("body")
-  .appendChild(drinkForm({ drinks }));
+function drinkForm({ drinks }) {
+  let addButton = create(
+    "button",
+    { type: "button", className: "add-button" },
+    "+ Добавить напиток"
+  );
+
+  let form = create(
+    "form",
+    {},
+    ...drinks.map((o, i) => singleDrinkForm({ id: i + 1, ...o })),
+    create("div", {}, addButton),
+    create(
+      "div",
+      { style: "margin-top: 30px" },
+      create("button", { type: "submit", className: "submit-button" }, "Готово")
+    )
+  );
+
+  return form;
+}
+
+function singleDrinkForm({ id, types, milk, options }) {
+  let fieldSet = create(
+    "fieldset",
+    { className: "beverage" },
+    create("h4", (props = { className: "beverage-count" }), `Напиток №${id}`),
+    create(
+      "label",
+      { className: "field" },
+      create("span", { className: "label-text" }, "Я буду"),
+      create(
+        "select",
+        {},
+        ...types.values.map((o) =>
+          option(
+            { value: o.value, selected: o.value === types.selected },
+            (v) => (types.selected = v),
+            create("span", {}, o.text)
+          )
+        )
+      )
+    ),
+    checkboxList(
+      "radio",
+      milk,
+      (props, v) => (props.selected = v),
+      (props, v) => props.selected === v
+    ),
+    checkboxList(
+      "checkbox",
+      options,
+      (props, v) =>
+        props.selected.includes(v)
+        ? props.selected.remove(v)
+        : props.selected.push(v),
+      (props, v) => props.selected.includes(v)
+    )
+  );
+  return fieldSet;
+}
+
+function checkboxList(inputType, props, changeSelected, checkSelected) {
+  return create(
+    "div",
+    { className: "field" },
+    checkboxLabel(props.label),
+    ...props.values.map((o) =>
+      create(
+        "label",
+        { className: "checkbox-field" },
+        input(
+          {
+            type: inputType,
+            name: props.name,
+            value: o.value,
+            checked: checkSelected(props, o.value),
+          },
+          (v) => changeSelected(props, v)
+        ),
+        create("span", {}, o.text)
+      )
+    )
+  );
+}
+
+function checkboxLabel(text) {
+  return create("span", { className: "checkbox-label" }, text);
+}
+
+/**
+ * @param {HTMLElementTagNameMap['input']} props
+ * @param {Function} onCheck
+ */
+function input(props, onCheck, ...children) {
+  let elem = create("input", props, ...children);
+  elem.addEventListener("change", (e) => onCheck(e.target.value));
+  return elem;
+}
+
+/**
+ * @param {HTMLElementTagNameMap['option']} props
+ * @param {Function} onCheck
+ */
+function option(props, onCheck, ...children) {
+  let elem = create("option", props, ...children);
+  elem.addEventListener("change", (e) => onCheck(e.target.value));
+  return elem;
+}
+
+/**'
+ * @param {keyof HTMLElementTagNameMap} tagName
+ * @param {HTMLElementTagNameMap[tagName]} props
+ * @param {...HTMLElement[]} children
+ * @returns {HTMLElementTagNameMap[tagName]}
+ */
+function create(tagName, props, ...children) {
+  let elem = document.createElement(tagName);
+  Object.keys(props).forEach((x) => (elem[x] = props[x]));
+  elem.append(...children);
+  return elem;
+}
+
+drinks = [{ ...defaultDrink }];
+
+let body = document.querySelector("body");
+
+let form = drinkForm({ drinks });
+
+body.appendChild(form);
+
+document.querySelector('.add-button').addEventListener('click', _ => {
+  drinks.push({...defaultDrink});
+  form.remove();
+  body.appendChild((drinkForm({drinks})));
+});
